8a745e44ee71a523a679114774a96a44b8e08a03
Lex Darlog (DRL) <drl.i3x@gmail.com>
2021-11-18 23:43:07 +0300

    + human_bytes() + format_thousands()

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 """
+Very generic stuff shared between modules.
 """
 
 __author__ = 'Lex Darlog (DRL)'
@@ -9,6 +10,9 @@ import typing as _t
 import abc as _abc
 from pathlib import Path
 from dataclasses import dataclass as _dataclass
+
+
+root_dir = Path(__file__).parent
 
 
 @_dataclass(init=False, frozen=True)
@@ -48,4 +52,64 @@ class CustomHash(_abc.ABC):
 		return hash(self.hash_id)
 
 
-root_dir = Path(__file__).parent
+_byte_kilo_sizes = ('', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi')
+_byte_yobi = 'Yi'
+_byte_kilo_step = 1024.0
+
+
+def human_bytes(
+	size: _t.Union[int, float], size_in_bits=False, space=' ', suffix='B'
+):
+	"""Format bytes/bits size in a human-readable form."""
+
+	def format_decimal(size_):
+		return f'{size_:.3f}'.rstrip('0').rstrip('.')
+
+	sign = -1 if size < 0 else 1
+	size *= sign  # make absolute
+	if size_in_bits:
+		size = size / 8
+
+	for unit in _byte_kilo_sizes:
+		if size < _byte_kilo_step:
+			return f'{format_decimal(size * sign)}{space}{unit}{suffix}'
+		size /= _byte_kilo_step
+
+	# We're in yobibytes.
+	if size < _byte_kilo_step:
+		return f'{format_decimal(size * sign)}{space}{_byte_yobi}{suffix}'
+
+	# OK, it's ridiculous now, but we're _AT_LEAST_ in thousands of yobibytes.
+	# Let's output in scientific notation.
+
+	# The function can still crash if a user provided number so big that it utilizes
+	# long math and can't be casted to a float. But if they deal with THAT order
+	# of sizes, it would be the least of their problems.
+	order = 0
+	while size >= _byte_kilo_step:
+		size /= _byte_kilo_step
+		order += 3
+	return f'{format_decimal(size * sign)}*10^{order}{space}{_byte_yobi}{suffix}'
+
+
+def format_thousands(number: _t.Union[int, float, str], spacer="'"):
+	"""Format a number, splitting thousands with spacer."""
+
+	number_str = str(number).replace(',', '.').strip()
+	sign = ''
+	if number_str.startswith('-'):
+		sign = '-'
+		number_str = number_str.lstrip('-')
+
+	if '.' in number_str:
+		number_str = number_str.rstrip('0').rstrip('.')
+	number_str = number_str.lstrip('0')
+
+	if not number_str:
+		return '0'
+
+	reversed_pieces = list()
+	while number_str:
+		number_str, piece = number_str[:-3], number_str[-3:]
+		reversed_pieces.append(piece)
+	return sign + spacer.join(reversed(reversed_pieces))
