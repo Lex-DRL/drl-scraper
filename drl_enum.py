79b2645605c44d537336f5bf9062bcfb5a160729
Lex Darlog (DRL) <drl.i3x@gmail.com>
2021-11-18 07:54:42 +0300

    drl_enum: better keep of inheritance (only methods defined in the decorated enum itself are still lost)

--- drl_enum.py
+++ drl_enum.py
@@ -6,6 +6,10 @@ __author__ = 'Lex Darlog (DRL)'
 from enum import *
 from itertools import chain as _chain
 
+from common import (
+	remove_duplicates as _remove_duplicates,
+)
+
 from drl_typing import *
 
 
@@ -23,50 +27,86 @@ def _enum_call_kwargs(enum_cls: _t_enum):
 	return enum_kwargs
 
 
-def _enum_base(enum_cls: _t_enum) -> _t_enum:
-	"""Extract base class the Enum was created from."""
-	base_enums = [
-		x for x in enum_cls.__bases__ if issubclass(x, Enum)
-	]
-	if not base_enums:
-		base_enums = [
-			x for x in enum_cls.__bases__ if isinstance(x, EnumMeta)
-		]
-	base_cls = base_enums[-1] if base_enums else Enum
-	assert isinstance(base_cls, EnumMeta)
-	# noinspection PyTypeChecker
-	return base_cls
+_t_enum_base = _u[_t_enum, type]
+_t_enum_bases = _tpl[_t_enum_base, ...]
+
+
+def _enum_bases(enum_cls: _t_enum) -> _t_enum_bases:
+	"""
+	Base classes for a given ``Enum``. If it doesn't inherit enything explicitly
+	(uses only meta-class), the default ``Enum`` is used.
+	"""
+	default = (Enum, )
+	try:
+		res = enum_cls.__bases__
+	except:
+		res = default
+	# keep only actual types:
+	res = tuple(x for x in res if isinstance(x, type))
+
+	if not any(issubclass(x, Enum) for x in res):
+		# it doesn't inherit the built-in Enum, force it to be the first base class:
+		res = tuple(*_chain(default, res))
+	# remove duplicates:
+	return tuple(_remove_duplicates(res))
+
+
+def _enum_constructing_base_class(
+	*out_enum_bases: _t_enum_base,
+	out_meta: _t_enum_meta,
+) -> _t_enum:
+	"""
+	Extract (or generate an intermediate) base ``Enum`` class used to create
+	a decorated ``Enum``.
+	"""
+	base_first = out_enum_bases[0]
+
+	if len(out_enum_bases) == 1:
+		assert issubclass(base_first, Enum), (
+			f"{base_first} is a single Enum base class "
+			f"but doesn't inherit from Enum itself"
+		)
+		# The given enum_cls has only one base and it's an Enum.
+		# Therefore (by definition) it's also an instance of EnumMeta.
+		# However, enum_cls itself might use a different meta from it's base.
+		# So we need to check for it:
+		if out_meta == _enum_base_meta(base_first):
+			# no need to define any intermediate classes:
+			return base_first
+
+	# unfortunately, we do need to define a constructing class...
+	class ModifiedBaseEnum(*out_enum_bases, metaclass=out_meta):
+		pass
+	return ModifiedBaseEnum
 
 
 def _enum_base_meta(enum_cls: _t_enum) -> _t_enum_meta:
 	"""Extract base meta-class the Enum was created from."""
-	base_meta = None
 	# noinspection PyBroadException
 	try:
-		base_meta = enum_cls.__class__
-		ok = True
+		res = enum_cls.__class__
 	except:
-		ok = False
-	if not base_meta:
-		ok = False
+		res = None
+	if not isinstance(res, type):
+		res = EnumMeta
 
-	if not ok:
-		base_metas = [
-			x for x in enum_cls.__bases__ if issubclass(x, EnumMeta)
-		]
-		base_meta = base_metas[-1] if base_metas else EnumMeta
+	if issubclass(res, EnumMeta):
+		return res
 
-	assert issubclass(base_meta, EnumMeta)
-	# noinspection PyTypeChecker
-	return base_meta
+	# Bad luck. We do have subclass, but it's not a child of EnumMeta.
+	# we need to create a multi-inherited meta-class:
+	bases = (EnumMeta, res)
+
+	class MergedEnumMeta(*_remove_duplicates(bases)):
+		pass
+	MergedEnumMeta.__name__ = res.__name__
+	return MergedEnumMeta
 
 
 def extend(*items, **kw_items):
 	"""Decorator which adds extra members to Enum."""
 
 	def decorator(enum_cls: _t_enum) -> _t_enum:
-		base_cls = _enum_base(enum_cls)
-
 		joined = dict(items)
 		joined.update(kw_items)
 		for k, v in items:
@@ -77,7 +117,10 @@ def extend(*items, **kw_items):
 		for k in joined.keys():
 			if k.startswith('__'):
 				joined.pop(k)
-		# noinspection PyArgumentList
+
+		base_cls = _enum_constructing_base_class(
+			*_enum_bases(enum_cls), out_meta=_enum_base_meta(enum_cls)
+		)
 		return base_cls(enum_cls.__name__, joined, **_enum_call_kwargs(enum_cls))
 
 	return decorator
@@ -112,7 +155,9 @@ def map_all_cases(
 		if unknown and default is None:
 			default_f = default_f_for_unknown
 
-		class ModifiedMeta(_enum_base_meta(enum_cls)):
+		base_meta = _enum_base_meta(enum_cls)
+
+		class ModifiedMeta(base_meta):
 			def __members_map_all_keys(cls):
 				try:
 					return cls.__map_all_keys
@@ -155,9 +200,7 @@ def map_all_cases(
 				if key in mapping:
 					return mapping[key]
 				return default_f(cls)
-
-		class ModifiedBaseEnum(_enum_base(enum_cls), metaclass=ModifiedMeta):
-			pass
+		ModifiedMeta.__name__ = base_meta.__name__
 
 		joined = dict()
 		if unknown:
@@ -168,7 +211,10 @@ def map_all_cases(
 			if k.startswith('__'):
 				joined.pop(k)
 
-		return ModifiedBaseEnum(
+		base_cls = _enum_constructing_base_class(
+			*_enum_bases(enum_cls), out_meta=ModifiedMeta
+		)
+		return base_cls(
 			enum_cls.__name__, joined, **_enum_call_kwargs(enum_cls)
 		)
 
